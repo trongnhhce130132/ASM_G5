b989e4756290c6275fa4e5e8c3b0d6ded43968de
trongan123 <62749635+trongan123@users.noreply.github.com>
2022-03-19 20:03:00 +0700

    code form user

--- ASMWPF/CartForm.xaml.cs
+++ ASMWPF/CartForm.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ASMLibrary.DataAccess;
+using ASMLibrary.Management.Sevice;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,23 +21,45 @@ namespace ASMWPF
     /// </summary>
     public partial class CartForm : Window
     {
-        public CartForm()
+        private GioHangService gioHangService = new GioHangService();
+        private MonAnService monAnService = new MonAnService();
+        private ChiTietDonHangService chiTietDonHangService = new ChiTietDonHangService();
+        private DonHangService donHangService = new DonHangService();
+        public KhachHang khachHang;
+        List<GioHang> gioHangs;
+        public CartForm(KhachHang khach)
         {
             InitializeComponent();
+            khachHang = khach;
+            Loaddata();
+        }
+        public void Loaddata()
+        {
+            
+            gioHangs =  gioHangService.GetGioHangByIDKhachHang(khachHang.Idkh).ToList();
+            decimal? gia = 0;
+            List<CartData> carts = new List<CartData>();
+            foreach(GioHang g in gioHangs)
+            {
+                carts.Add(new CartData { Id = g.Idmon, Name=monAnService.GetMonAnByID(g.Idmon).TenMon , Soluong=g.SoLuong, Price =  (int)g.DonGia+"VND" });
+                gia += g.DonGia*g.SoLuong;
+            }
+            lvCart.ItemsSource = carts;
+            lbgia.Content =(int)gia+"VND";
         }
 
         private void btnUser_Click(object sender, RoutedEventArgs e)
         {
-            //UserForm user = new UserForm();
-            //user.Show();
-            //this.Close();
+            UserForm user = new UserForm(khachHang);
+            user.Show();
+            this.Close();
         }
 
         private void btnHome_Click(object sender, RoutedEventArgs e)
         {
-            //HomePageForm home = new HomePageForm();
-            //home.Show();
-            //this.Close();
+            HomePageForm home = new HomePageForm(khachHang);
+            home.Show();
+            this.Close();
         }
 
         private void btnLogout_Click(object sender, RoutedEventArgs e)
@@ -43,6 +67,97 @@ namespace ASMWPF
             LoginForm login = new LoginForm();
             login.Show();
             this.Close();
+        }
+
+        private void lvCart_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (lvCart.SelectedIndex != -1)
+            {
+                MessageBoxResult messageBoxResult = MessageBox.Show("Are you sure?", "Delete food Confirmation", MessageBoxButton.YesNo);
+                if (messageBoxResult == MessageBoxResult.Yes)
+                {
+                    try
+                    {
+                        GioHang gio = gioHangs[lvCart.SelectedIndex];
+
+                        gioHangService.DeleteGioHang(gio);
+                        Loaddata();
+                        MessageBox.Show("Delete Info Success!!");
+                    }
+                    catch (Exception)
+                    {
+
+                        throw;
+                    }
+
+                }
+            }
+        }
+
+        private void btnOrder_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult messageBoxResult = MessageBox.Show("Do you want to order?", "Order food Confirmation", MessageBoxButton.YesNo);
+            if (messageBoxResult == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    DonHang don = new DonHang();
+                    don.IddonHang = donHangService.GetIDCuoi();
+                    don.Idkh = khachHang.Idkh;
+                    don.Tt = 0;
+                    donHangService.AddDonHang(don);
+                    foreach (GioHang g in gioHangs)
+                    {
+                        ChiTietDonHang ctDon = new ChiTietDonHang();
+                        ctDon.Idctdh = chiTietDonHangService.GetIDCuoi();
+                        ctDon.IddonHang = don.IddonHang;
+                        ctDon.Idmon = g.Idmon;
+                        ctDon.SoLuong = g.SoLuong;
+                        ctDon.DonGia = g.DonGia;
+                        ctDon.Tt = 1;
+                        chiTietDonHangService.AddChiTietDonHang(ctDon);
+                    }
+                    OrderForm orderForm = new OrderForm(khachHang, don);
+                    orderForm.Show();
+                    this.Close();
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+
+            }
+        }
+    }
+
+    public class CartData
+    {
+        private string _Id;
+        private string _Name;
+        private int? _Soluong;
+        private string _Price;
+
+        public string Id
+        {
+            get { return _Id; }
+            set { _Id = value; }
+        }
+        public string Name
+        {
+            get { return _Name; }
+            set { _Name = value; }
+        }
+
+        public int? Soluong
+        {
+            get { return _Soluong; }
+            set { _Soluong = value; }
+        }
+        public string Price
+        {
+            get { return _Price; }
+            set { _Price = value; }
         }
     }
 }
--- ASMWPF/OrderForm.xaml.cs
+++ ASMWPF/OrderForm.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ASMLibrary.DataAccess;
+using ASMLibrary.Management.Sevice;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +21,143 @@ namespace ASMWPF
     /// </summary>
     public partial class OrderForm : Window
     {
-        public OrderForm()
+
+        private MonAnService monAnService = new MonAnService();
+        private ChiTietDonHangService chiTietDonHangService = new ChiTietDonHangService();
+        private DonHangService donHangService = new DonHangService();
+        public KhachHang khachHang;
+        public DonHang donHang;
+        decimal? gia = 0;
+        List<ChiTietDonHang> chiTietDons;
+        public OrderForm(KhachHang khach, DonHang don)
         {
             InitializeComponent();
+            khachHang = khach;
+            donHang = don;
+            txtAddress.Text = khachHang.DiachiKh;
+            txtPhone.Text = khachHang.Sdtkh;
+            Loaddata();
+        }
+
+        public void Loaddata()
+        {
+
+            chiTietDons = chiTietDonHangService.GetChiTietDonHangByIDDonHang(donHang.IddonHang).ToList();
+           
+            
+            List<CartData> carts = new List<CartData>();
+            foreach (ChiTietDonHang g in chiTietDons)
+            {
+                carts.Add(new CartData { Id = g.Idmon, Name = monAnService.GetMonAnByID(g.Idmon).TenMon, Soluong = g.SoLuong, Price = (int)g.DonGia + "VND" });
+                gia += g.DonGia * g.SoLuong;
+            }
+            lvOrder.ItemsSource = carts;
+            lbgia.Content = (int)gia + "VND";
+        }
+
+       
+
+        private void lvOrder_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (lvOrder.SelectedIndex != -1)
+            {
+                MessageBoxResult messageBoxResult = MessageBox.Show("Are you sure?", "Delete food Confirmation", MessageBoxButton.YesNo);
+                if (messageBoxResult == MessageBoxResult.Yes)
+                {
+                    try
+                    {
+                        ChiTietDonHang ctdon = chiTietDons[lvOrder.SelectedIndex];
+
+                        chiTietDonHangService.DeleteChiTietDonHang(ctdon);
+                       
+                        MessageBox.Show("Delete Info Success!!");
+                        Loaddata();
+                    }
+                    catch (Exception)
+                    {
+
+                        throw;
+                    }
+
+                }
+            }
+        }
+
+        private void Rectangle_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            HomePageForm homePageForm = new HomePageForm(khachHang);
+            homePageForm.Show();
+            this.Close();
+
+        }
+
+       
+
+        private void btnorder_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult messageBoxResult = MessageBox.Show("Do you want to order?", "Order food Confirmation", MessageBoxButton.YesNo);
+            if (messageBoxResult == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    var now = DateTime.Now;
+         
+
+                    donHang.Diachi = txtAddress.Text;
+                    donHang.Sdt = txtPhone.Text;
+                    donHang.TongTien = gia;
+                    donHang.Tt = 1;
+                    donHang.ThoiGian = now;
+                    donHangService.UpdateDonHang(donHang);
+
+                    MessageBox.Show("You onder Success!!");
+
+                    HomePageForm homePageForm = new HomePageForm(khachHang);
+                    homePageForm.Show();
+                    this.Close();
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+
+            }
+        }
+    }
+
+
+
+
+
+
+    public class OrderData
+    {
+        private string _Id;
+        private string _Name;
+        private int? _Soluong;
+        private string _Price;
+
+        public string Id
+        {
+            get { return _Id; }
+            set { _Id = value; }
+        }
+        public string Name
+        {
+            get { return _Name; }
+            set { _Name = value; }
+        }
+
+        public int? Soluong
+        {
+            get { return _Soluong; }
+            set { _Soluong = value; }
+        }
+        public string Price
+        {
+            get { return _Price; }
+            set { _Price = value; }
         }
     }
 }
